9de3a428e54f17f3fc4b91ad7c004e1c41c660fa
YoyoMy <35624284+YoyoMy@users.noreply.github.com>
2019-03-03 17:22:27 +0200

    Update raspberryPyCode.py

--- raspberryPyCode.py
+++ raspberryPyCode.py
@@ -1,6 +1,6 @@
-Python 3.5.3 (default, Sep 27 2018, 17:25:39) 
-[GCC 6.3.0 20170516] on linux
-Type "copyright", "credits" or "license()" for more information.
+#Python 3.5.3 (default, Sep 27 2018, 17:25:39) 
+#[GCC 6.3.0 20170516] on linux
+#Type "copyright", "credits" or "license()" for more information.
 >>> import time
 >>> import Adafruit_DHT as dht
 >>> import MySQLdb
